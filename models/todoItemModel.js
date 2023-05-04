870eb6daf85bbe82739d83393ec30db9bbb91406
MAlDibawy <mahmoudaldibawy@gmail.com>
2023-05-05 01:55:57 +0300

    add user model and env.example

--- models/todoItemModel.js
+++ models/todoItemModel.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const TodoItemSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
     item: {
         type: String,
         required: true
